8f76220ca4bf53a549d124c0e0dff4db4ec75e05
Mario Grullon <mitgrullon@hotmail.com>
2016-12-15 21:52:51 -0400

    Changed datepicker from bootstrap to semantic ui

--- app/assets/javascripts/date_range_picker.js
+++ app/assets/javascripts/date_range_picker.js
@@ -3,8 +3,15 @@ date_range_picker = function() {
   $('.date-range-picker').each(function(){
     $(this).daterangepicker({
         timePicker: true,
-        timePickerIncrement: 30,
-        alwaysShowCalendars: true
+        timePickerIncrement: 15,
+        alwaysShowCalendars: true,
+        locale: {
+                applyLabel: 'Aplicar',
+                cancelLabel: 'Cancelar',
+                fromLabel: 'Desde',
+                toLabel: 'Hasta',
+                weekLabel: 'S',
+                customRangeLabel: 'Rango'}
     }, function(start, end, label) {
       $('.start_hidden').val(start.format('YYYY-MM-DD HH:mm'));
       $('.end_hidden').val(end.format('YYYY-MM-DD HH:mm'));
