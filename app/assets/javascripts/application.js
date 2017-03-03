7afd4d46b73c444a472cf1b41fe7ed32728b0bd8
Mario Grullon <mitgrullon@hotmail.com>
2017-03-03 09:59:19 -0400

    updating

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery-ui
 //= require turbolinks
 //= require semantic-ui
 //= require lib/momentjs/moment
@@ -19,7 +20,7 @@
 //= require lib/daterangepicker
 //= require lib/calendar
 //= require tablesort
-//= require_tree .
+
 
 
 
