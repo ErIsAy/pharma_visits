6eaad9125a90e2608a46a261f3965e9dd01d49a5
Mario Grullon <mitgrullon@hotmail.com>
2017-03-03 09:56:45 -0400

    updating assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,17 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-
+//= require jquery
+//= require jquery_ujs
+//= require jquery-ui
+//= require turbolinks
+//= require semantic-ui
+//= require lib/momentjs/moment
+//= require lib/fullcalendar/fullcalendar
+//= require lib/daterangepicker
+//= require lib/calendar
+//= require tablesort
+//= require_tree .
 
 
 
