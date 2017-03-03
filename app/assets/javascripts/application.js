4c836060f413f5af169e79102a91869ac1089454
Mario Grullon <mitgrullon@hotmail.com>
2017-03-03 10:08:31 -0400

    removing semantic

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,8 +14,12 @@
 //= require jquery_ujs
 //= require jquery-ui
 //= require turbolinks
-//= require semantic-ui
-
+//= require lib/momentjs/moment
+//= require lib/fullcalendar/fullcalendar
+//= require lib/daterangepicker
+//= require lib/calendar
+//= require tablesort
+//= require_tree .
 
 /************fadeOut messages ***********/
 $(function() {
