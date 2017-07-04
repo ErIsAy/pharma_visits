100e38d3fc0c892c4fcfceca0f020a8ad40c6af6
Mario Grullon <mario.grullon@minted.com>
2017-07-04 00:27:31 -0400

    Added import for admin panel and misc updates

--- app/models/center.rb
+++ app/models/center.rb
@@ -1,10 +1,12 @@
 # No Doc
 class Center < ActiveRecord::Base
-  validates :name, presence: true, uniqueness: true
+  # validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
+
   has_many :doctors
 
   CITIES =
-    [ ['Todas', ''], 
+    [ ['Todas', ''],
       ['Azua', 'Azua'],
       ['Baní', 'Baní'],
       ['Barahona', 'Barahona'],
