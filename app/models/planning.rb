d7060ed17aaa07b58f8f056f630f3383b002215a
Mario Grullon <mitgrullon@hotmail.com>
2018-03-23 20:09:31 -0400

    added dependent destroy to planning visits

--- app/models/planning.rb
+++ app/models/planning.rb
@@ -3,7 +3,7 @@ class Planning < ActiveRecord::Base
 belongs_to :user
 belongs_to :doctor
 belongs_to :cycle
-has_many :visits
+has_many :visits, :dependent => :destroy
 validates :title, presence: true
 validates :shift, presence: true
 
