5d196fec727bffd06a607fbbd731a4a7860001fe
Mario Grullon <mitgrullon@hotmail.com>
2018-04-02 20:03:58 -0400

    fixed visited mark when cycle is created and visit is registered

--- app/models/cycle.rb
+++ app/models/cycle.rb
@@ -1,3 +1,17 @@
 class Cycle < ActiveRecord::Base
   has_many :plannings
+  after_create :clean_plannings_visit
+
+
+
+
+def clean_plannings_visit
+  Planning.all.each do |x|
+    x.visited = false
+    # byebug
+    x.save
+  end
+end
+
+
 end
