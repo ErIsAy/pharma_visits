2c60d4472df8571c596993f44582c2cf3a27479a
Mario Grullon <mitgrullon@hotmail.com>
2018-06-21 17:05:42 -0400

    added select day in the planning form

--- app/models/planning.rb
+++ app/models/planning.rb
@@ -9,11 +9,38 @@ validates :doctor, presence: true
 # validates :shift, presence: true
 # validates :date_visit, presence: true
 
-    SHIFTS =
-      [
-        ['AM', 'AM'],
-        ['PM', 'PM']
+  SHIFTS =
+    [
+      ['AM', 'AM'],
+      ['PM', 'PM']
 
-      ]
+    ]
+
+  DAYS =
+  [
+    ['Semana 1 - Lunes', 0],
+    ['Semana 1 - Martes', 1],
+    ['Semana 1 - Miercoles', 2],
+    ['Semana 1 - Jueves', 3],
+    ['Semana 1 - Viernes', 4],
+
+    ['Semana 2 - Lunes', 7],
+    ['Semana 2 - Martes', 8],
+    ['Semana 2 - Miercoles', 9],
+    ['Semana 2 - Jueves', 10],
+    ['Semana 2 - Viernes', 11],
+
+    ['Semana 3 - Lunes', 14],
+    ['Semana 3 - Martes', 15],
+    ['Semana 3 - Miercoles', 16],
+    ['Semana 3 - Jueves', 17],
+    ['Semana 3 - Viernes', 18],
+
+    ['Semana 4 - Lunes', 21],
+    ['Semana 4 - Martes', 22],
+    ['Semana 4 - Miercoles', 23],
+    ['Semana 4 - Jueves', 24],
+    ['Semana 4 - Viernes', 25]
+  ]  
 
 end
