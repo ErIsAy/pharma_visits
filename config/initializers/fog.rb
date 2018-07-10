43f75136c454d14d10b7b19acce46dd18f9c40a8
Mario Grullon <mitgrullon@hotmail.com>
2018-07-10 18:40:44 -0400

    updated env variables

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -2,9 +2,9 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider:              'AWS',
-    aws_access_key_id:     'AKIAIEKYJPLZULHQTZ3A',
-    aws_secret_access_key: '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',
-    region:                'us-east-1'
+    aws_access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),,
+    region:                ENV.fetch('AWS_REGION'),
   }
-  config.fog_directory  = 'gupharma'                                      # required
+  config.fog_directory  = ENV.fetch('S3_BUCKET_NAME')                                      # required
 end
