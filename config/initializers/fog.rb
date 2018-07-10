ab19be1eae30245968ea3a4543d172321822dd77
Mario Grullon <mitgrullon@hotmail.com>
2018-07-10 18:42:21 -0400

    syntax error fix

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -3,8 +3,8 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:              'AWS',
     aws_access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
-    aws_secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),,
+    aws_secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
     region:                ENV.fetch('AWS_REGION'),
   }
-  config.fog_directory  = ENV.fetch('S3_BUCKET_NAME')                                      # required
+  config.fog_directory  = ENV.fetch('S3_BUCKET_NAME')
 end
