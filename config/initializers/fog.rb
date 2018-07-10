756cc3e3b5adddeef55ea92611c6c859de725c67
Mario Grullon <mitgrullon@hotmail.com>
2018-07-10 17:46:53 -0400

    updating aws

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider:              'AWS',
-    aws_access_key_id:     'AKIAJDMUEOWOT5NQ5H7A',
-    aws_secret_access_key: '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',
+    aws_access_key_id:     'AKIAIEKYJPLZULHQTZ3A',
+    aws_secret_access_key: '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',
     region:                'us-east-1'
   }
   config.fog_directory  = 'gupharma'                                      # required
